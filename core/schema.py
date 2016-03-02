d0eb56ab704ba267a0645ccd8f682d68d8bbd524
zimmer <zimmer@fysik.su.se>
2016-03-02 18:40:54 +0100

    move to pymongo & marrow.mongo

---
+++ core/schema.py
@@ -0,0 +1,40 @@
+'''
+Created on Mar 2, 2016
+
+@author: zimmer
+special thanks to GothAlice in the #mongodb freenode channel
+'''
+
+import datetime, random
+from flask import url_for
+
+from marrow.mongo.core.document import Document
+from marrow.mongo.field.registry import DateTime, String, Long
+
+
+class Job(Document):
+    release = String(required=False)
+    taskName = String(required=True)
+    particle = String(required=True)
+    type = String(default=None, choices=("None", "Generator", "Digitization", "Reconstruction"))
+    @property
+    def created_at(self):
+        return self._id.generation_time
+    def get_id(self):
+        return self._id
+    
+    def get_absolute_url(self):
+        return url_for('job', kwargs={"taskName": self.taskName})
+    
+    def __unicode__(self):
+        return self.taskName
+
+
+class JobInstance(Job):
+    randomSeed = Long(default=0)
+    
+    def get_absolute_url(self):
+        return url_for('job', kwargs={"taskName": self.taskName})
+    
+    def __unicode__(self):
+        return unicode(self._id)
