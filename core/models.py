9dd3971091009da20ba57d0ddd31f213ebcec15a
zimmer <zimmer@fysik.su.se>
2016-03-15 17:51:04 +0100

    initial state machinery

--- core/models.py
+++ core/models.py
@@ -2,11 +2,13 @@ import datetime
 from flask import url_for
 from core import db
 
+MAJOR_STATII = ('New','Running','Failed','Terminated','Done','Submitted')
 
 class JobInstance(db.EmbeddedDocument):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
     body = db.StringField(verbose_name="JobInstance", required=True)
     author = db.StringField(verbose_name="Name", max_length=255, required=True)
+    status = db.StringField(verbose_name="status", required=False, default="New", choices=MAJOR_STATII)
 
 class Job(db.Document):
     created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
---
+++ scripts/dampe-cli-fetch-new-jobs.py
@@ -0,0 +1,5 @@
+'''
+Created on Mar 15, 2016
+
+@author: zimmer
+'''
