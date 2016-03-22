819d1ad23c7b019b8c040e62b9f009d5a692e57b
zimmer <zimmer@fysik.su.se>
2016-03-22 12:27:41 +0100

    enumerate!

--- core/lsf.py
+++ core/lsf.py
@@ -22,7 +22,7 @@ class lsf(object):
         keys = "USER,STAT,QUEUE,FROM_HOST,EXEC_HOST,JOB_NAME,\
                 SUBMIT_TIME,PROJ_NAME,CPU_USED,MEM,SWAP,PIDS,START_TIME,FINISH_TIME,SLOTS".split(",")
         output = run(command)
-        for i, line in output.split("\n"):
+        for i, line in enumerate(output.split("\n")):
             if i>0:
                 this_line = line.split(" ")
             jobs[this_line[0]]=dict(zip(keys,this_line[1,-1]))
