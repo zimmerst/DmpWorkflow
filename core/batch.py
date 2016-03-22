da2b6df79ebc2b6a8b5736a62477897a519cf2e0
zimmer <zimmer@fysik.su.se>
2016-03-22 13:54:19 +0100

    more general batch implementation

--- core/batch.py
+++ core/batch.py
@@ -6,23 +6,37 @@ Created on Mar 22, 2016
 from core.shell import run
 import logging
 
-class lsf(object):
+class batch(object):
     '''
     classdocs
     '''
     allJobs = {}
-    keys = "USER,STAT,QUEUE,FROM_HOST,EXEC_HOST,JOB_NAME,"
-    keys+= "SUBMIT_TIME,PROJ_NAME,CPU_USED,MEM,SWAP,PIDS,START_TIME,FINISH_TIME,SLOTS"
-    keys = keys.split(",")
-        
-    def __init__(self, params):
+    keys = []
+    
+    def __init__(self):
         '''
         Constructor
         '''
         allJobs = self.update()
+
+    def update(self):
+        return {}
+    
+    def getJob(self,jobID,key="STAT",callable=str):
+        if not jobID in self.allJobs:
+            logging.error("could not find job %s"%jobID)
+        if not key in self.keys:
+            logging.error("could not extract key, allowed keys %s"%str(self.keys))
+        return callable(self.allJobs[jobID][key])   
+
+# LSF-specific stuff
+class lsf(batch):
+    keys = "USER,STAT,QUEUE,FROM_HOST,EXEC_HOST,JOB_NAME,"
+    keys+= "SUBMIT_TIME,PROJ_NAME,CPU_USED,MEM,SWAP,PIDS,START_TIME,FINISH_TIME,SLOTS"
+    keys = keys.split(",")
     
     def update(self):
-        self.allJobs.update(self.aggregateStatii(asDict))
+        self.allJobs.update(self.aggregateStatii())
     
     def aggregateStatii(self,asDict=True,command=["bjobs -Wa"]):
         jobs = {}
@@ -39,9 +53,3 @@ class lsf(object):
                     if len(this_job):
                         jobs[jobID]=this_job
             return jobs
-    def getJob(self,jobID,key="STAT",callable=str):
-        if not jobID in self.allJobs:
-            logging.error("could not find job %s"%jobID)
-        if not key in self.keys:
-            logging.error("could not extract key, allowed keys %s"%str(self.keys))
-        return callable(self.allJobs[jobID][key])   
