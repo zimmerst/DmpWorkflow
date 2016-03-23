a2f0d34aa08c8e987f4d8965bb1ead1d1f49bfe3
zimmer <zimmer@fysik.su.se>
2016-03-23 12:16:38 +0100

    convenience functions added.

--- hpc/batch.py
+++ hpc/batch.py
@@ -8,7 +8,7 @@ import logging
 
 class BATCH(object):
     '''
-    classdocs
+    generic Batch class, all HPC-specific modules should inherit from it.
     '''
     allJobs = {}
     keys = []
@@ -21,11 +21,28 @@ class BATCH(object):
 
     def update(self):
         return {}
-    
+
+    def __checkKeys__(self,key):
+        if not key in self.keys:
+            logging.error("could not extract key, allowed keys %s"%str(self.keys))
+            raise Exception
     def getJob(self,jobID,key="STAT",callable=str):
         if not jobID in self.allJobs:
             logging.error("could not find job %s"%jobID)
-        if not key in self.keys:
-            logging.error("could not extract key, allowed keys %s"%str(self.keys))
+        self.__checkKeys__(key)
         return callable(self.allJobs[jobID][key])   
-
+    
+    def getAttributeForAllJobs(self,attr="MEM"):
+        ''' convenience function to return all values for a certain attribute '''
+        key = attr
+        self.__checkKeys__(key)
+        ret = {}
+        for jobID in self.allJobs:
+            val = None
+            if not self.allJobs[jobID].has_key(key):
+                logging.error("could not find key %s in job %s"%(key,jobID))
+            else:
+                val = self.allJobs[jobID][key]
+            ret[jobID]=val
+        return ret
+    
