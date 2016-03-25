f7f560057b166b25d7514319996646da7cc5c628
zimmer <zimmer@fysik.su.se>
2016-03-25 16:32:41 +0100

    broad updates

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,7 @@ Created on Mar 10, 2016
 
 @author: zimmer
 '''
-from flask import Blueprint, request, redirect, render_template, url_for
+from flask import Blueprint, request, redirect, render_template, url_for, current_app
 from flask.views import MethodView
 
 from flask.ext.mongoengine.wtf import model_form
@@ -12,7 +12,6 @@ from core.auth import requires_auth
 from core.models import Job, JobInstance
 
 admin = Blueprint('admin', __name__, template_folder='templates')
-
 
 class List(MethodView):
     decorators = [requires_auth]
@@ -29,7 +28,7 @@ class Remove(MethodView):
         if slug:
             job = Job.objects.get_or_404(slug=slug);
             job.delete();
-            
+            current_app.logger.info("removing job %s"%slug)
         return redirect(url_for('admin.index'))
 
 class Detail(MethodView):
@@ -43,8 +42,10 @@ class Detail(MethodView):
             job = Job.objects.get_or_404(slug=slug)
             if request.method == 'POST':
                 form = form_cls(request.form, inital=job._data)
+                current_app.logger.info("POST request from form")
             else:
                 form = form_cls(obj=job)
+                current_app.logger.info("other request")
         else:
             job = Job()
             form = form_cls(request.form)
@@ -76,5 +77,6 @@ class Detail(MethodView):
 # Register the urls
 admin.add_url_rule('/admin/', view_func=List.as_view('index'))
 admin.add_url_rule('/admin/create/', defaults={'slug': None}, view_func=Detail.as_view('create'))
-admin.add_url_rule('/admin/<slug>/', view_func=Detail.as_view('edit'))
+admin.add_url_rule('/admin/edit/<slug>/', view_func=Detail.as_view('edit'))
 admin.add_url_rule('/admin/remove/<slug>/', view_func=Remove.as_view('remove'))
+
