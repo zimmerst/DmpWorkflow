b8ed7d3d471b73a15fa0832add3f102f658023e0
zimmer <zimmer@fysik.su.se>
2016-04-20 01:02:15 +0200

    minor changes.

--- scripts/dampe-execute-payload.py
+++ scripts/dampe-execute-payload.py
@@ -36,7 +36,7 @@ log.debug("current environment settings {}".format(os.environ))
 for fi in job.InputFiles:
     src = os.path.expandvars(fi['source'])
     tg  = os.path.expandvars(fi['target'])
-    log.info("cp %s ==> %s"%(src,tg))
+    log.info("cp %s >>> %s"%(src,tg))
     try:
         safe_copy(src, tg, attempts=4, sleep='4s')
     except IOError, e:
@@ -64,7 +64,7 @@ job.updateStatus("Running","PreparingOutputData")
 for fi in job.OutputFiles:
     src = os.path.expandvars(fi['source'])
     tg  = os.path.expandvars(fi['target'])
-    log.info("cp %s -> %s"%(src,tg))
+    log.info("cp %s >>> %s"%(src,tg))
     try:
         safe_copy(src, tg, attempts=4, sleep='4s')
     except IOError, e:        
