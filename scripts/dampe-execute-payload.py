15a0559583369eee560a202696bdcfc3d5264787
zimmer <zimmer@fysik.su.se>
2016-04-20 00:43:24 +0200

    disable debugger.

--- scripts/dampe-execute-payload.py
+++ scripts/dampe-execute-payload.py
@@ -10,7 +10,7 @@ from utils.tools import mkdir, safe_copy, rm, camelize, exceptionHandler
 from utils.shell import run
 import sys, os, logging, pprint, socket
 
-DEBUG_TEST = True
+DEBUG_TEST = False
 
 LOG_LEVEL = os.getenv("LOG_LEVEL","INFO")
 if not LOG_LEVEL=="DEBUG":
