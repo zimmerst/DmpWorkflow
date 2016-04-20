ce26ce8531214476d8e6e8c155c9786b9144b3cc
sposs <steph.poss@gmail.com>
2016-04-20 22:20:05 +0200

    start fixing

--- scripts/dampe-cli-create-new-job.py
+++ scripts/dampe-cli-create-new-job.py
@@ -1,47 +1,52 @@
-'''
+"""
 Created on Mar 30, 2016
 
 @author: zimmer
 @brief: prototype script to create a new job from the jobXml
-'''
-from utils.scriptDefaults import cfg
+"""
+
 from core.models import Job, JobInstance, TYPES
 from utils.flask_helpers import parseJobXmlToDict
 from core import db
-import random, sys, os
+import sys, os
 from ptyprocess.ptyprocess import FileNotFoundError
+from argparse import ArgumentParser
 
-_TYPES = list(TYPES)+["NONE"]
+_TYPES = list(TYPES) + ["NONE"]
 
-dummy_dict = {"InputFiles":[],"OutputFiles":[],"MetaData":[]}
+dummy_dict = {"InputFiles": [], "OutputFiles": [], "MetaData": []}
 
-if __name__ == '__main__':
-    from optparse import OptionParser
-    parser = OptionParser()
-    usage = "Usage: %prog taskName xmlFile [options]"
-    description = "create new instances for job in DB"
-    parser.set_usage(usage)
-    parser.set_description(description)
-    (opts, arguments) = parser.parse_args()
-    #if len(sys.argv)!=3:
+
+def main(args=None):
+    parser = ArgumentParser(usage="Usage: %prog taskName xmlFile [options]",
+                            description="create new instances for job in DB")
+    opts = parser.parse_args(args)
+    # if len(sys.argv)!=3:
     #    print parser.print_help()
     #    raise Exception
-    taskName        = sys.argv[1]
-    ninst         = int(sys.argv[2])
+    taskName = sys.argv[1]
+    ninst = int(sys.argv[2])
     db.connect()
     job = Job.objects.filter(title=taskName)
     if len(job):
-        os.environ['DWF_JOBNAME']=job.title
+        os.environ['DWF_JOBNAME'] = job.title
         dout = parseJobXmlToDict(job.body)
-        if 'type' in dout['atts']: job.type = dout['atts']['type']
-        if 'release' in dout['atts']: job.release = dout['atts']['release']
-        if not opts.type == "NONE": job.type = opts.type
+        if 'type' in dout['atts']:
+            job.type = dout['atts']['type']
+        if 'release' in dout['atts']:
+            job.release = dout['atts']['release']
+        if not opts.type == "NONE":
+            job.type = opts.type
         if ninst:
             for j in range(ninst):
                 jI = JobInstance(body=str(dummy_dict))
                 job.addInstance(jI)
-        #print len(job.jobInstances)
+        # print len(job.jobInstances)
         job.save()
-        print 'added %i new instances for job %s'%(ninst,taskName)
-    else: 
-        raise FileNotFoundError('could not find job %s'%taskName)
+        print 'added %i new instances for job %s' % (ninst, taskName)
+    else:
+        raise FileNotFoundError('could not find job %s' % taskName)
+
+
+if __name__ == "__main__":
+    main()
