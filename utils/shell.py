40fb3ebb473d3248f1c1e23e26c9bfeed334c122
zimmer <zimmer@fysik.su.se>
2016-04-20 19:56:29 +0200

    remove `BASH_FUNC_module'

--- utils/shell.py
+++ utils/shell.py
@@ -32,7 +32,7 @@ def source_bash(setup_script):
     for l in lines: 
         tl = l.split("=")
         if len(tl)==2:
-            if not tl[0].startswith("_"):
+            if not tl[0].startswith("_") or tl[0]=="BASH_FUNC_module":
                 keys.append(tl[0])
                 values.append(tl[1])
     os.environ.update(dict(zip(keys,values)))
