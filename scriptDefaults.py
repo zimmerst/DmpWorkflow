b9ebce7d4aca2169c199fe66568d50ee2f4360a5
zimmer <zimmer@fysik.su.se>
2016-04-20 19:41:32 +0200

    no virtualenv, but install flask in system python.

--- scriptDefaults.py
+++ scriptDefaults.py
@@ -22,18 +22,20 @@ cfg = ConfigParser.SafeConfigParser(defaults=myDefaults)
 cfg.read(os.getenv("WorkflowConfig","config/dampe.cfg"))
 os.environ["DAMPE_SW_DIR"]=cfg.get("site","DAMPE_SW_DIR")
 
-print "seting up externals"
+#print "seting up externals"
 source_bash(cfg.get("site","ExternalsScript"))
 
 WorkflowRoot = os.getenv("DWF_ROOT",os.getenv("DAMPE_SW_DIR"))
-print "ROOT workflow: %s"%WorkflowRoot
+#print "ROOT workflow: %s"%WorkflowRoot
 
 pwd = os.getenv("PWD",".")
-print "current path %s"%os.path.abspath(pwd)
+#print "current path %s"%os.path.abspath(pwd)
 
-print "seting up flask"
-activate_this_file = os.path.expandvars("${DWF_ROOT}/bin/activate_this.py")
-print 'calling execfile(%s)'%activate_this_file
-execfile(activate_this_file, dict(__file__=activate_this_file))
+#print "seting up flask"
+#activate_this_file = os.path.expandvars("${DWF_ROOT}/bin/activate")
+#os.system('/bin/bash --rcfile %s'%activate_this_file)
+
+#print 'calling execfile(%s)'%activate_this_file
+#execfile(activate_this_file, dict(__file__=activate_this_file))
 
 ## done with that.
