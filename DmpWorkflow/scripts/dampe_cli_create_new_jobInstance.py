540db2ada3c7e5ed7f425eb0b945fee56e513768
zimmer <zimmer@fysik.su.se>
2016-04-25 18:38:10 +0200

    cleanup.

--- DmpWorkflow/scripts/dampe_cli_create_new_jobInstance.py
+++ DmpWorkflow/scripts/dampe_cli_create_new_jobInstance.py
@@ -20,14 +20,9 @@ def main(args=None):
     usage = "Usage: %(prog)s taskName xmlFile [options]"
     description = "create new instances for job in DB"
     parser = ArgumentParser(usage=usage, description=description)
-    # parser.add_option("--instance", dest="inst",type=int, default = None,
-    #                  help='use this to offset an instance')
     parser.add_argument("-n", "--name", help="task name", dest="name")
     parser.add_argument("-i", "--instances", help="number of instances", dest="inst", type=int)
     opts = parser.parse_args(args)
-    # if len(sys.argv)!=3:
-    #    print parser.print_help()
-    #    raise Exception
     taskName = opts.name
     os.environ['DWF_JOBNAME'] = taskName
     ninst = opts.inst
