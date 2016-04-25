5be4fd7ba3d4e648badcbac70fd833a8e4d54d40
zimmer <zimmer@fysik.su.se>
2016-04-25 16:27:09 +0200

    fix.

--- DmpWorkflow/core/views.py
+++ DmpWorkflow/core/views.py
@@ -1,7 +1,7 @@
 import copy
 import json
 import logging
-import requests.exceptions.HTTPError as HTTPError
+import requests.exceptions as exceptions
 from flask import Blueprint, request, redirect, render_template, url_for
 from flask.ext.mongoengine.wtf import model_form
 from flask.views import MethodView
@@ -85,7 +85,7 @@ class JobView(MethodView):
             # print len(job.jobInstances)
             job.save()
             return json.dumps({"result": "ok", "jobID": str(job.id)})
-        except HTTPError, ex:
+        except exceptions.HTTPError, ex:
             logger.exception(ex)
 
 class JobInstanceView(MethodView):
