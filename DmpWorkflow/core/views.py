b0cce60eb2e911abf59bc07a9134648721eb32c4
zimmer <zimmer@fysik.su.se>
2016-04-25 16:02:47 +0200

    fix.

--- DmpWorkflow/core/views.py
+++ DmpWorkflow/core/views.py
@@ -60,7 +60,7 @@ class JobView(MethodView):
         return "Nothing to display"
 
     def post(self):
-        logger.warning(str(request.__dict__))
+        print str(request.__dict__)
         taskname = request.form['taskname']
         jobdesc = request.files['file']
         type = request.form['type']
