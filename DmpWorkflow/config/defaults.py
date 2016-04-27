e3b20fd7ecabc0a1d33ee08eb7af3119dcf28697
zimmer <zimmer@fysik.su.se>
2016-04-27 23:37:49 +0200

    improve logging.

--- DmpWorkflow/config/defaults.py
+++ DmpWorkflow/config/defaults.py
@@ -16,6 +16,8 @@ def AppLogger(name,level=logging.INFO):
     log = logging.getLogger(name)
     log.setLevel(logging.INFO)
     ch = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
     log.addHandler(ch)
     return log
 
