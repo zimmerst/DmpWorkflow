d9951c1ef0b99f3d7e1a2ebe4f36f4e22c9a252f
zimmer <zimmer@fysik.su.se>
2016-04-29 23:35:58 +0200

    fix.

--- DmpWorkflow/core/models.py
+++ DmpWorkflow/core/models.py
@@ -54,7 +54,7 @@ class Job(db.Document):
         return None
 
     def addInstance(self, jInst, inst=None):
-        if len(self.jobInstances>=1000000):
+        if len(self.jobInstances)>=1000000:
             raise Exception("reached maximum of job instances, consider cloning this job instead.")
         if not isinstance(jInst, JobInstance):
             log.exception("must be job instance to be added")
