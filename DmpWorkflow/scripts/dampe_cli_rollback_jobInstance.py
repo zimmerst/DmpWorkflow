6bba993c77ce8ba9b31a530091bc884182a77760
zimmer <zimmer@fysik.su.se>
2016-04-29 15:04:34 +0200

    only prompt if query returned anything.

--- DmpWorkflow/scripts/dampe_cli_rollback_jobInstance.py
+++ DmpWorkflow/scripts/dampe_cli_rollback_jobInstance.py
@@ -14,7 +14,7 @@ def main(args=None):
     usage = "Usage: %(prog)s JobName [options]"
     description = "roll-back Jobs in DB"
     parser = ArgumentParser(usage=usage, description=description)
-    parser.add_argument("--title", dest="title", type=str, default=None, help='name of job', required=True)
+    parser.add_argument("--title", dest="title", type=str, default=None, help='name of job', required=False)
     parser.add_argument("--instance", dest="inst", type=int, default=None, help='to roll back specific instance', required=False)
     parser.add_argument("--status", dest="stat", type=str, default="Failed", help='jobs to be picked for roll-back', required=False)
     parser.add_argument("--n_min", dest="n_min", type=int, default=None, help='roll back everything above this number', required=False)
@@ -31,22 +31,24 @@ def main(args=None):
     if res.get("result","nok") != "ok":
         print "error %s" % res.get("error")
     jobs = res.get("jobs")
-    print 'found %i jobs that satisfy query conditions.'%len(jobs)
-    if query_yes_no("continue rolling back %i instances?"%len(jobs)):
-        for j in jobs:
-            my_dict = {"t_id": j['jobId'], "inst_id": j['instanceId'], 
-                       "major_status": "New", "minor_status":"AwaitingBatchSubmission", "hostname":None,
-                       "batchId":None, "status_history":[], 
-                       "log": "", "cpu":None, "memory":None, "created_at":datetime.datetime.now()}
-            res = requests.post("%s/jobstatus/" % DAMPE_WORKFLOW_URL, data={"args": json.dumps(my_dict)})
-            res.raise_for_status()
-            res = res.json()
-            if not res.get("result", "nok") == "ok":
-                print ("error resetting instance %s"%res.get("error"))
-        print 'rolled back %i instances'%len(jobs)
+    if len(jobs):
+        print 'found %i jobs that satisfy query conditions.'%len(jobs)
+        if query_yes_no("continue rolling back %i instances?"%len(jobs)):
+            for j in jobs:
+                my_dict = {"t_id": j['jobId'], "inst_id": j['instanceId'], 
+                           "major_status": "New", "minor_status":"AwaitingBatchSubmission", "hostname":None,
+                           "batchId":None, "status_history":[], 
+                           "log": "", "cpu":None, "memory":None, "created_at":datetime.datetime.now()}
+                res = requests.post("%s/jobstatus/" % DAMPE_WORKFLOW_URL, data={"args": json.dumps(my_dict)})
+                res.raise_for_status()
+                res = res.json()
+                if not res.get("result", "nok") == "ok":
+                    print ("error resetting instance %s"%res.get("error"))
+            print 'rolled back %i instances'%len(jobs)
+        else:
+            print 'rollback aborted'
     else:
-        print 'rollback aborted'
-        
+        print 'could not find any jobs satisfying the query.'
 
 
 if __name__ == '__main__':
