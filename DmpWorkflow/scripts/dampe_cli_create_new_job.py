c5d9bf73482396ed6761029e830ee4129de26eb8
zimmer <zimmer@fysik.su.se>
2016-05-01 20:41:43 +0200

    xml parse before uploading and update option parser dictionary.

--- DmpWorkflow/scripts/dampe_cli_create_new_job.py
+++ DmpWorkflow/scripts/dampe_cli_create_new_job.py
@@ -5,21 +5,28 @@ Created on Mar 30, 2016
 @brief: prototype script to create a new job from the jobXml
 """
 import requests
+from os.path import isfile
+import os.path as oP
 from argparse import ArgumentParser
 from DmpWorkflow.config.defaults import DAMPE_WORKFLOW_URL, TYPES, SITES
+from DmpWorkflow.utils.tools import parseJobXmlToDict
 
 def main(args=None):
     parser = ArgumentParser(usage="Usage: %(prog)s taskName xmlFile [options]", description="create new job in DB")
     parser.add_argument("-t", "--type", dest="t_type", type=str, default="User", help='task type', choices=TYPES)
     parser.add_argument("--Ninstances", dest="Ninstances", type=int, default=0,
                         help='number of instances to create at the same time')
-    parser.add_argument("-i", "--input", dest="xml", help="Path to job XML")
+    parser.add_argument("-i", "--input", dest="xml", help="Path to job XML", required=True)
     parser.add_argument("-n", '--name', help="task Name", dest="tname")
     parser.add_argument("-s", '--site', help="site to run at", dest="site", default='local', required=True, choices=SITES)
     opts = parser.parse_args(args)
+    xmlFile = unicode(opts.xml)
+    assert isfile(opts.xml), "must be an accessible file."
+    xdict = parseJobXmlToDict(open(opts.xml,"r").read())
+    atts = xdict['atts']
+    opts.update(atts)
     assert opts.site in SITES, "site not supported in DB %s"%opts.site
     taskName = unicode(opts.tname)
-    xmlFile = unicode(opts.xml)
     t_type = unicode(opts.t_type)
     n_instances = int(opts.Ninstances)
     site = unicode(opts.site)
