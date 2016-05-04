e790a0b489f55eddda4d3d48bc81aea101752d3b
zimmer <zimmer@fysik.su.se>
2016-05-04 17:11:35 +0200

    do logging from dictionary defaults.

---
+++ DmpWorkflow/config/logger.py
@@ -0,0 +1,41 @@
+'''
+Created on May 4, 2016
+
+@author: zimmer
+'''
+from DmpWorkflow.config.defaults import cfg
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+                        "precise": {
+                                    "format": "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+                                    'datefmt': '%Y-%m-%d %H:%M:%S'
+                                    }                           
+                       },
+        'handlers': {
+                        'mail_admins': {
+                                        'level': 'INFO',
+                                        'class': 'django.utils.log.AdminEmailHandler'
+                                        },
+                        'console':     {
+                                        'level': 'DEBUG',
+                                        'class': 'logging.StreamHandler',
+                                        },
+                        'file':         {
+                                         'level': "INFO",
+                                         'formatter': "precise",
+                                         'class': 'logging.handlers.RotatingFileHandler',
+                                         'filename': cfg.get("server","logfile"),
+                                         'maxBytes': "2000000",
+                                         'backupCount': 5
+                                         }
+                    },
+        'loggers': {
+                    'app': {
+                            'handlers': ['console', "file"],
+                            'level': 'INFO',
+                            },
+                    }
+               }
--- DmpWorkflow/core/__init__.py
+++ DmpWorkflow/core/__init__.py
@@ -1,14 +1,10 @@
+from DmpWorkflow.core.config.logger import LOGGING
 import logging
-from logging.handlers import RotatingFileHandler
-
 from DmpWorkflow.config.defaults import cfg
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
 kind = cfg.get("global","installation")
-LOG_FILENAME = cfg.get("server","logfile")
-
-
 if kind == 'server':
     app = Flask(__name__)
     app.config['MONGODB_DB'] = cfg.get("database", "name")
@@ -16,15 +12,7 @@ if kind == 'server':
     app.config['MONGODB_PASSWORD'] = cfg.get("database", "password")
     app.config['MONGODB_HOST'] = cfg.get("database", "host")
     app.config["SECRET_KEY"] = "KeepThisS3cr3t"
- 
-    formatter = logging.Formatter(
-        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
-    handler = RotatingFileHandler(LOG_FILENAME, maxBytes=10000000, backupCount=5)
-    debug = cfg.getboolean("server","use_debugger")
-    if debug: handler.setLevel(logging.DEBUG)
-    else: handler.setLevel(logging.WARNING)
-    handler.setFormatter(formatter)
-    app.logger.addHandler(handler)
+    app.logger.config.dictConfig(LOGGING)
     
     db = MongoEngine(app)
     
