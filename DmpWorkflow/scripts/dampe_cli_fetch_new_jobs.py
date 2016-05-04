448ea5f2089cab8eee6f5e2dcb1aacf1f11f6252
zimmer <zimmer@fysik.su.se>
2016-05-04 19:01:25 +0200

    add limit to how many jobs should be submitted to site.

--- DmpWorkflow/scripts/dampe_cli_fetch_new_jobs.py
+++ DmpWorkflow/scripts/dampe_cli_fetch_new_jobs.py
@@ -4,6 +4,7 @@ Created on Mar 15, 2016
 @author: zimmer
 """
 import requests
+import sys
 from argparse import ArgumentParser
 from DmpWorkflow.core.DmpJob import DmpJob
 from DmpWorkflow.config.defaults import DAMPE_WORKFLOW_URL, BATCH_DEFAULTS, AppLogger
@@ -14,9 +15,20 @@ def main(args=None):
     parser.add_argument("-l", "--local", dest="local", action = 'store_true', default=False, help='run locally')
     parser.add_argument("-p", "--pythonbin", dest="python", default=None, type=str, help='the python executable if non standard is chosen')
     parser.add_argument("-c", "--chunk", dest="chunk", default=100, type=int, help='number of jobs to process per cycle')
+    parser.add_argument("-m", "--maxJobs", dest="maxJobs", default=None, type=int, help='number of jobs that can be in the system')
     opts = parser.parse_args(args)
     log = AppLogger("dampe-cli-fetch-new-jobs")
     batchsite = BATCH_DEFAULTS['name']
+    if not opts.maxJobs is None:
+        res = requests.get("%s/watchdog/" % DAMPE_WORKFLOW_URL, data = {"site":str(batchsite)})
+        res.raise_for_status()
+        res = res.json()
+        if not res.get("result", "nok") == "ok":
+            log.error(res.get("error"))
+        jobs = res.get("jobs")
+        if len(jobs) >= opts.maxJobs:
+            log.info("reached maximum number of jobs per site, not submitting anything, change this value by setting it to higher value")
+            sys.exit();
     res = requests.get("%s/newjobs/" % DAMPE_WORKFLOW_URL, data = {"site":str(batchsite)})
     res.raise_for_status()
     res = res.json()
