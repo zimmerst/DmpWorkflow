2f7de3761def58e723ec4a617935351063d642bf
zimmer <zimmer@fysik.su.se>
2016-05-10 17:34:59 +0200

    fix.

--- DmpWorkflow/scripts/dampe_cli_run_watchdog.py
+++ DmpWorkflow/scripts/dampe_cli_run_watchdog.py
@@ -40,6 +40,7 @@ def __updateStatus(job, batchId, mem, cpu, batchEngine = None, dry=True):
     elif stat in FINAL_STATII:
         if job['major_status']!=stat:
             log.warning("found a job that should be in non-final state but batch reports it to be failed or done, updating db")
+    else: return
     log.info("about to call update with this data %s",my_dict)
     if not dry:
         res = requests.post("%s/jobstatus/" % DAMPE_WORKFLOW_URL, data={"args": json.dumps(my_dict)})
