d2db5c1fe6e697cd650244896a7e46d2250d7481
zimmer <zimmer@fysik.su.se>
2016-05-18 14:12:23 +0200

    adding some server-scripts.

---
+++ DmpWorkflow/scripts/removeDuplicates.py
@@ -0,0 +1,48 @@
+'''
+Created on May 18, 2016
+
+@author: zimmer
+@brief: server-side script to remove duplicates.
+'''
+import progressbar
+from DmpWorkflow.core.models import JobInstance
+
+def main():
+    try:
+        from DmpWorkflow.core import db
+        db.connect()
+    except Exception as err:
+        import sys
+        print 'could not import DB, is this configuration a server setup?'
+        sys.exit(0)
+    instances = JobInstance.objects.all()
+    print 'found %i instances'%len(instances)
+    pb = progressbar.ProgressBar(maxval=len(instances))
+    
+    duplicates = []
+    good_instances = []
+    pb.start()
+    for i,inst in enumerate(instances):
+        if inst.instanceId in good_instances:
+            duplicates.append(inst)
+        else:
+            good_instances.append(inst.instanceId)
+        pb.update(i+1)
+    pb.finish()
+    print 'found %i duplicates'%len(duplicates)
+    if len(duplicates):
+        print 'removing...'
+        pb = progressbar.ProgressBar(maxval=len(duplicates))
+        pb.start()
+    for i, inst in enumerate(duplicates):
+        # do cleanup
+        ref_instance = inst.job.getInstance(inst.instanceId)
+        if ref_instance.id != inst.id:
+            #print inst.instanceId
+            inst.delete()
+        pb.update(i+1)
+    pb.finish()
+    print 'done'
+
+if __name__ == "__main__":
+    main()
