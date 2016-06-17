ed8a10dff63837100711df9639b45c9b17d0a437
zimmer <zimmer@fysik.su.se>
2016-06-17 18:14:57 +0200

    fix.

--- DmpWorkflow/scripts/dampe_cli_create_new_jobInstance.py
+++ DmpWorkflow/scripts/dampe_cli_create_new_jobInstance.py
@@ -22,7 +22,7 @@ def main(args=None):
     override_dict = {"InputFiles": [], "OutputFiles": [], "MetaData": []}
     if opts.set_var is not None:
         var_dict = dict({tuple(val.split("=")) for val in opts.set_var.split(";")})
-        override_dict['MetaData']=[{"name":k,"value":v,"type":str} for k,v in var_dict.iteritems()]
+        override_dict['MetaData']=[{"name":k,"value":v,"type":"str"} for k,v in var_dict.iteritems()]
     taskName = opts.name
     environ['DWF_JOBNAME'] = taskName
     ninst = opts.inst
