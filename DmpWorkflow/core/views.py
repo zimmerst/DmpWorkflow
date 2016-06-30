c1bf0e06b8a73486d9e4cc9d52d56b8a62873e7c
zimmer <zimmer@fysik.su.se>
2016-06-30 11:25:49 +0200

    must set body if you override...

--- DmpWorkflow/core/views.py
+++ DmpWorkflow/core/views.py
@@ -263,7 +263,7 @@ class SetJobStatus(MethodView):
                     del arguments['minor_status']
                 if major_status != oldStatus:
                     jInstance.setStatus(major_status)
-                    #jInstance.setBody(bdy)
+                    jInstance.setBody(bdy)
                     if 'body' in arguments: del arguments['body']
                 for key in ["t_id","inst_id","major_status"]: del arguments[key]
                 for key,value in arguments.iteritems():
