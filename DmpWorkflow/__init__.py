e828327fb772e83373a60fa1eb00cecfca8a0601
sposs <steph.poss@gmail.com>
2016-07-05 22:16:35 +0200

    little missed file

--- DmpWorkflow/__init__.py
+++ DmpWorkflow/__init__.py
@@ -1,23 +1,25 @@
+import logging
+
 # Define Version
 
 majorVersion = 0
 minorVersion = 1
-patchLevel   = 0
-preVersion   = 1
+patchLevel = 0
+preVersion = 1
 
-version      = "v%sr%s" % ( majorVersion, minorVersion )
-buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
+version = "v%sr%s" % (majorVersion, minorVersion)
+buildVersion = "v%dr%d" % (majorVersion, minorVersion)
 if patchLevel:
-    version = "%sp%s" % ( version, patchLevel )
-    buildVersion = "%s build %s" % ( buildVersion, patchLevel )
+    version = "%sp%s" % (version, patchLevel)
+    buildVersion = "%s build %s" % (buildVersion, patchLevel)
 if preVersion:
-    version = "%s-pre%s" % ( version, preVersion )
-    buildVersion = "%s pre %s" % ( buildVersion, preVersion )
+    version = "%s-pre%s" % (version, preVersion)
+    buildVersion = "%s pre %s" % (buildVersion, preVersion)
 
-from DmpWorkflow.utils.logger import initLogger
-import logging
 try:
     from DmpWorkflow.config.defaults import DAMPE_LOGFILE
+    from DmpWorkflow.utils.logger import initLogger
+
     initLogger(DAMPE_LOGFILE)
 except Exception as err:
     logging.warning("Log service client was not initialized properly: %s", str(err))
