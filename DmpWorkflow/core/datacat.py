926e02a89478dcc9256f496aa13b18fc8db29bb5
zimmer <zimmer@fysik.su.se>
2016-07-05 14:41:36 +0200

    rudimentary object structure.

--- DmpWorkflow/core/datacat.py
+++ DmpWorkflow/core/datacat.py
@@ -10,49 +10,60 @@ import logging
 from datetime import datetime
 import sys
 from mongoengine import CASCADE
-from copy import deepcopy
-from flask import url_for
-from ast import literal_eval
+from os.path import join as op_join
+#from copy import deepcopy
+#from flask import url_for
+#from ast import literal_eval
 #from StringIO import StringIO
-from DmpWorkflow.config.defaults import cfg, MAJOR_STATII, FINAL_STATII, TYPES, SITES
+from DmpWorkflow.config.defaults import cfg #, MAJOR_STATII, FINAL_STATII, TYPES, SITES
 from DmpWorkflow.core import db
-from DmpWorkflow.utils.tools import random_string_generator, exceptionHandler
-from DmpWorkflow.utils.tools import parseJobXmlToDict, convertHHMMtoSec, sortTimeStampList
+#from DmpWorkflow.utils.tools import random_string_generator, exceptionHandler
+#from DmpWorkflow.utils.tools import parseJobXmlToDict, convertHHMMtoSec, sortTimeStampList
 
 if not cfg.getboolean("site", "traceback"): sys.excepthook = exceptionHandler
 log = logging.getLogger("core")
 
 class DataReplica(db.Document):
-    site   = db.StringField(max_length=24, required=True)
-    status = db.StringField(max_length=16, default="New")
-    filename= db.StringField(max_length=1024, required=True)
-    DataFile= db.ReferenceField("DataFile", reverse_delete_rule=CASCADE)
+    site     = db.StringField(max_length=24, required=True)
+    status   = db.StringField(max_length=16, default="New")
+    path     = db.StringField(max_length=1024, required=True)
+    checksum = db.StringField(max_length=72, required=False)
+    DataFile = db.ReferenceField("DataFile", reverse_delete_rule=CASCADE)
+    meta     = {
+                 'allow_inheritance': True,
+                 'indexes': ['-created_at', 'status', 'site'],
+                 'ordering': ['-created_at']
+               }
 
-    def setStatus(self,stat):
-        if stat not in self.my_choices:
-            raise Exception("status not supported in DB")
-        self.status = stat
+    def getFileName(self):
+        return op_join(self.path,self.DataFile.filename)
 
 class DataFile(db.Document):
-    my_choices = ("New","Copied","Orphaned")
     created_at = db.DateTimeField(default=datetime.now, required=True)
+    dataset = db.ReferenceField("DataSet", reverse_delete_rule=CASCADE)
     replicas   = db.ListField(db.ReferenceField("DataReplica")) 
+    filename= db.StringField(max_length=1024, required=True)
     filetype = db.StringField(max_length=16, required=False, default="root")
+    # here are attributes specific to the datafile
     tstart = db.DateTimeField(required=False)
     tstop  = db.DateTimeField(required=False)
 
-    def save(self):
-        req = DataFile.objects.filter(filename=self.filename, site=self.site)
-        if req:
-            raise Exception("a file with the specified properties exists already, consider updating instead!")
-        super(DataFile, self).save()
+    meta = {
+            'allow_inheritance': True,
+            'indexes': ['-created_at', 'filename', 'site', 'filetype'],
+            'ordering': ['-created_at']
+            }
 
-    def update(self):
-        log.debug("calling update on DataFile")
-        super(DataFile, self).save()
+class DataSet(db.Document):
+    created_at = db.DateTimeField(default=datetime.now, required=True)
+    files = db.ListField(db.ReferenceField("DataFile"))
+    release = db.StringField(max_length=64, required=False)
+    name = db.StringField(max_length=128,required=True)
+    filetype = db.StringField(max_length=16, required=False, default="root")
 
     meta = {
             'allow_inheritance': True,
-            'indexes': ['-created_at', 'filename', 'site'],
+            'indexes': ['-created_at', 'release', 'name'],
             'ordering': ['-created_at']
             }
+    
