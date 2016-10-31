59e981abf4fea64a0b5a86a076bf1993b5698897
zimmer <zimmer@fysik.su.se>
2016-10-31 23:08:27 +0100

    minor update to reaper

--- DmpWorkflow/scripts/reaper.py
+++ DmpWorkflow/scripts/reaper.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 
 def main(args=None):
     parser = ArgumentParser(usage="Usage: %(prog)s [options]", description="reap instances which have not send any heartbeat")
-    parser.add_argument('-t','--time',type=int,default=30,dest='time',help='timedelta in minutes to be used by reaper to kill jobs')
+    parser.add_argument('-t','--time',type=int,default=360,dest='time',help='timedelta in minutes to be used by reaper to kill jobs')
     parser.add_argument('-d','--dry',action='store_true',dest='dry',help='do not reap but show what you would reap (dry-run)')
     opts = parser.parse_args(args)
     past = datetime.now() - timedelta(minutes=opts.time)
