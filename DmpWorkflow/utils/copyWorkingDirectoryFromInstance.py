0fcf5ec581d80ae7fd07b76cd2131d390b6f50da
David Francois Droz <david.droz@outlook.com>
2018-02-08 15:11:15 +0100

    Added options for gridFTP transfer

--- DmpWorkflow/utils/copyWorkingDirectoryFromInstance.py
+++ DmpWorkflow/utils/copyWorkingDirectoryFromInstance.py
@@ -38,6 +38,8 @@ def main(args=None):
   parser.add_argument("--type","-t", dest="task_type", help="type of task, Simulation is default", default="Simulation")
   parser.add_argument("--site","-s", dest='site', default="BARI", help="Name of site the job was running at")
   parser.add_argument("--inst_id","-i", dest='inst_id', help="Instance ID", type=int,required=True)
+  parser.add_argument("--workdir_path", dest='wdpth', default="mc/workdir",help="Path of working directory in remote location")
+  parser.add_argument("--gridftp_path", dest='rmtpth', default='stzimmer@gridftp.cscs.ch/scratch/snx3000/stzimmer',help="Full path for gridFTP transfer")
   #parser.add_argument("--output","-o",dest='output',help='output directory, if not set, will create directory here',default=None)
   opts = parser.parse_args(args)
   #outdir=abspath(curdir)
@@ -45,10 +47,14 @@ def main(args=None):
   #  outdir=opts.output
   #if not isdir(outdir): mkdir(outdir) 
   if site == 'CSCS':
-	xrd_cmd = "globus-url-copy -r -sync -q -rst sshftp://stzimmer@gridftp.cscs.ch/scratch/snx3000/stzimmer/mc/workdir/{site}/{task}/{task_type}/{sixDigit} file://{cwd}/{task}/{inst}/. 2> /dev/null".format(site=opts.site, task=opts.task, sixDigit=getSixDigits(opts.inst_id,asPath=True),task_type=opts.task_type,cwd=getcwd())
+	xrd_cmd = "globus-url-copy -r -sync -q -rst sshftp://{remotepath}/{wdpath}/{site}/{task}/{task_type}/{sixDigit} file://{cwd}/{task}/{inst}/. 2> /dev/null".format(site=opts.site, task=opts.task, 
+																																									sixDigit=getSixDigits(opts.inst_id,asPath=True),
+																																									task_type=opts.task_type, cwd=getcwd(),
+																																									wdpath=opts.wdpth, remotepath=opt.rmtpth)
 	# r: recursive ; sync: no copy if file already exists; q: quiet mode; rst: restart if failed, up to 5 times
+  
   else:
-    xrd_files="xrdfs xrootd-dampe.cloud.ba.infn.it ls -u /UserSpace/dampe_prod/mc/workdir/{site}/{task}/{task_type}/{sixDigit} 2> /dev/null".format(site=opts.site, task=opts.task, sixDigit=getSixDigits(opts.inst_id,asPath=True),task_type=opts.task_type)
+    xrd_files="xrdfs xrootd-dampe.cloud.ba.infn.it ls -u /UserSpace/dampe_prod/{wdpath}/{site}/{task}/{task_type}/{sixDigit} 2> /dev/null".format(site=opts.site, task=opts.task, sixDigit=getSixDigits(opts.inst_id,asPath=True),task_type=opts.task_type,wdpath=opts.wdpth)
     xrd_cmd="{list} | xargs -I @ xrdcp @ {task}/{inst}/.".format(list=xrd_files,inst=opts.inst_id,task=opts.task)
   
   mkcmd="mkdir -pv {task}/{inst}".format(task=opts.task,inst=opts.inst_id)
